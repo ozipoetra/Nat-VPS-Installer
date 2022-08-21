c35b230b0ede66ac588d00b3eecd1035b8773c96
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2022-08-21 16:31:34 +0700

    Update install-nat.sh

--- install-nat.sh
+++ install-nat.sh
@@ -6,17 +6,21 @@ fi
 
 # xray service
 aa="https://raw.githubusercontent.com/ozipoetra/mantapv2/main/data-xray.sh"
-# sshws service
-cc="https://raw.githubusercontent.com/ozipoetra/Mantap/main/install-nat.sh"
-
 #installing script
 wget ${aa} && chmod +x data-xray.sh && ./data-xray.sh
+
+cd /usr/bin
+wget -O xraymenu https://raw.githubusercontent.com/ozipoetra/mantapv2/main/menu-akun/add-ws.sh
+chmod +x xraymenu
+cd /root
 clear
-wget ${cc} -O ninstall-nat.sh && chmod +x ninstall-nat.sh && ./ninstall-nat.sh
+
+wget -O ninstall-nat.sh https://raw.githubusercontent.com/ozipoetra/Mantap/main/install-nat.sh && chmod +x ninstall-nat.sh && ./ninstall-nat.sh
+
+#
 clear
 echo "Installation Success! Rebooting..."
 sleep 3
 reboot
-
 # clear installation files (soon)
 # rm -r *.sh
